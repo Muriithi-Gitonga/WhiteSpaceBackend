5fcda6f971dc1b5df120fc9a2de1eeae97a88424
Emanukim <emmanue.kimweli@student.moringaschool.com>
2023-01-11 12:19:51 +0300

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   post "/login", to: "supervisors#login"
   post "/signup", to: "supervisors#create"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
