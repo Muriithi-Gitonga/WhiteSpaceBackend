86f9e70e633789420aa6506df7d31d2a5447a818
Maureenkamau03 <mkaymaureen@gmail.com>
2023-01-12 16:58:52 +0300

    WHIT-7 added tests for password

--- spec/models/supervisor_spec.rb
+++ spec/models/supervisor_spec.rb
@@ -25,15 +25,23 @@ RSpec.describe Supervisor, type: :model do
       end
 
     end
+    # username
     describe "username" do
       it ("expects username to be present") do
         expect(Supervisor.create(name: "Maureen Kamau", email: "mkaymaureen@gmail.com", password_digest:"mkay1234")).to be_invalid
         expect(Supervisor.create(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen@gmail.com", password_digest:"mkay1234")).to be_valid
       end
       it "expects username to be unique" do
-        Supervisor.create(username:"Helloworld",password_digest:"hellothere")
-        expect(Supervisor.create(username:"Helloworld",password_digest:"hellothere")).to be_invalid
+        Supervisor.create(name:"Hello World", email: "hello@gmail.com",username:"Helloworld",password_digest:"hellothere")
+        expect(Supervisor.create(name:"Hello World", email: "hello@gmail.com",username:"Helloworld",password_digest:"hellothere")).to be_invalid
       end
+    end
+    # password
+    describe "password:" do
+      it "expects password to have a minimum of 8 characters" do
+        expect(Supervisor.create(name:"Test Pwd", email: "pwd@gmail.com", username:"Pwd",password_digest:"short")).to be_invalid
+        expect(Supervisor.create(name:"Test Pwd", email: "pwd@gmail.com",username:"Pwd",password_digest:"longenough")).to be_valid
+       end
     end
   end
 
