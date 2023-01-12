8bca7fc1fa0bb79cf6b893d87407091870b403bb
Maureenkamau03 <mkaymaureen@gmail.com>
2023-01-12 10:19:06 +0300

    WHIT-7 updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   post "/login", to: "supervisors#login"
   post "/signup", to: "supervisors#create"
+  get "/supervisors/:id", to: "supervisors#show"
+  get "/supervisors", to: "supervisors#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
