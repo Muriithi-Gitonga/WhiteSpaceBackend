7b3f8152441aec16e34d0796bae349a3909ca201
Maureenkamau03 <mkaymaureen@gmail.com>
2023-01-12 20:24:21 +0300

    WHIT-7 refactoring test code, adding email tests

--- app/models/supervisor.rb
+++ app/models/supervisor.rb
@@ -6,10 +6,10 @@ class Supervisor < ApplicationRecord
 
   # validates :password, presence: true, confirmation: true, length: { minimum: 8 }, password_validation: true
 
-  # validates :username, presence: true, uniqueness: true
-  # validates :name, presence: true
-  # validates :password, presence: true, length: { minimum: 8 }
-  # validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
 end
 # get jira commit message
--- spec/models/supervisor_spec.rb
+++ spec/models/supervisor_spec.rb
@@ -1,48 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Supervisor, type: :model do
-  DatabaseCleaner.strategy = :truncation
-    before(:each) do
-      DatabaseCleaner.clean
-    end
-  
-    after(:all) do
-      DatabaseCleaner.clean
-    end
+  DatabaseCleaner.strategy = :transaction
+  before(:each) do
+    DatabaseCleaner.start
+  end
+
+  after(:each) do
+    DatabaseCleaner.clean
+  end
 # properties
   describe "Properties" do
-    it "Verifies that Supervisor has name, username, email and password" do
-      expect(Supervisor.new.attributes).to include(:name, :username, :email, :password_digest)
+    it "Verifies that Supervisor has name, username, email and password_digest" do
+      expect(Supervisor.new.attributes.keys).to include("name", "username", "email", "password_digest")
     end
   end
 
-  # validations
-  describe "validations" do
-    describe "name" do
-      it "expects name to be present" do
-        expect(Supervisor.create(username: "Maureen", email: "mkaymaureen@gmail.com", password_digest: "mkay1234")).to be_invalid
-        expect(Supervisor.create(name: "Maureen Kamau", username: "Maureen", email: "mkaymaureen@gmail.com", password_digest: "mkay1234")).to be_valid
-      end
 
+  # validations
+  
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(Supervisor.new(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen@gmail.com", password: "mkay1234")).to be_valid
     end
-    # username
-    describe "username" do
-      it ("expects username to be present") do
-        expect(Supervisor.create(name: "Maureen Kamau", email: "mkaymaureen@gmail.com", password_digest:"mkay1234")).to be_invalid
-        expect(Supervisor.create(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen@gmail.com", password_digest:"mkay1234")).to be_valid
-      end
-      it "expects username to be unique" do
-        Supervisor.create(name:"Hello World", email: "hello@gmail.com",username:"Helloworld",password_digest:"hellothere")
-        expect(Supervisor.create(name:"Hello World", email: "hello@gmail.com",username:"Helloworld",password_digest:"hellothere")).to be_invalid
-      end
+  
+    it "is invalid without a name" do
+      expect(Supervisor.new(username: "Mkay", email: "mkaymaureen@gmail.com", password: "mkay1234")).to be_invalid
     end
-    # password
-    describe "password:" do
-      it "expects password to have a minimum of 8 characters" do
-        expect(Supervisor.create(name:"Test Pwd", email: "pwd@gmail.com", username:"Pwd",password_digest:"short")).to be_invalid
-        expect(Supervisor.create(name:"Test Pwd", email: "pwd@gmail.com",username:"Pwd",password_digest:"longenough")).to be_valid
-       end
+  
+    it "is invalid without a username" do
+      expect(Supervisor.new(name: "Maureen Kamau", email: "mkaymaureen@gmail.com", password: "mkay1234")).to be_invalid
     end
+  
+    it "is invalid with a duplicate username" do
+      Supervisor.create(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen@gmail.com", password: "mkay1234")
+      expect(Supervisor.new(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen1@gmail.com", password: "mkay1234")).to be_invalid
+    end
+  
+    it "is invalid without an email" do
+      expect(Supervisor.new(name: "Maureen Kamau", username: "Mkay", password: "mkay1234")).to be_invalid
+    end
+    it "is invalid with a duplicate email" do
+      Supervisor.create(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen@gmail.com", password: "mkay1234")
+      expect(Supervisor.new(name: "Maureen Kamau", username: "Mkay", email: "mkaymaureen1@gmail.com", password: "mkay1234")).to be_invalid
+    end
+
+    it "expects email to be in a valid format" do
+      expect(Supervisor.new(name: "Maureen Kamau", username: "Maureen", email: "invalid_email", password_digest: "mkay1234")).to be_invalid
+    end
+  
   end
 
 end
