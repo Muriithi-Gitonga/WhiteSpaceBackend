5ee96a420c5ca9406227cff2179255a175514ae8
Emanukim <emmanue.kimweli@student.moringaschool.com>
2023-01-16 09:07:23 +0300

    add client and task seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 lecturer = Lecturer.create([{name: "Mary Kimani", institution: "University of Nairobi"}, {name: "Gibson Bonareri", institution: "DayStar University"}, {name: "John Maina", institution: "Mt Kenya University"}])
 #Lecturer.create(name: "Mary Kimani").valid?
 #Lecturer.create(name: nil).valid?
-Profile.create!(bio:"male",username:"kim",supervisor_id:1, student_id:1)
-Profile.create!(bio:"male" ,username:"mike" ,supervisor_id:2, student_id:2)
-Profile.create!(bio:"female" ,username:"cindy" ,supervisor_id:3, student_id:3)
-Profile.create!(bio:"female" ,username:"joy" ,supervisor_id:1, student_id:4)
-Profile.create!(bio:"male" ,username:"omosh" ,supervisor_id:2, student_id:5)
-Profile.create!(bio:"male" ,username:"juma" ,supervisor_id:4, student_id:6)
-Profile.create!(bio:"male" ,username:"manu" ,supervisor_id:2, student_id:7)
+Profile.create!(bio:"male",username:"kim", student_id:1)
+Profile.create!(bio:"male" ,username:"mike" , student_id:2)
+Profile.create!(bio:"female" ,username:"cindy" , student_id:3)
+Profile.create!(bio:"female" ,username:"joy" , student_id:4)
+Profile.create!(bio:"male" ,username:"omosh" , student_id:5)
+Profile.create!(bio:"male" ,username:"juma" , student_id:6)
+Profile.create!(bio:"male" ,username:"manu" , student_id:7)
