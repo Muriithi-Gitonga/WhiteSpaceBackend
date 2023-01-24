49953fdaeb0a4c8940b0ec071bf60cd8f523029c
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-01-24 10:31:12 +0300

    configured for deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -32,6 +33,8 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  config.hosts << "WhiteSpaceBackend-production.up.railway.app"
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
---
+++ lib/tasks/fake-assets.rake
@@ -0,0 +1,6 @@
+namesapce :assets do
+    desc "Should just print stuff and move on. Fake!"
+    tasks :precompile do
+        puts "This is a Fake assets:precompile"
+    end
+end
